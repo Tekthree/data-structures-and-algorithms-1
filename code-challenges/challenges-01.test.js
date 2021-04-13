4a13f4739c13b31b674f73e0436328236f3c4a4a
Tek Jones <tekthree@gmail.com>
2021-04-13 10:00:20 -0700

    finished up challenge 7

--- code-challenges/challenges-01.test.js
+++ code-challenges/challenges-01.test.js
@@ -161,6 +161,25 @@ Return the resulting output array.
 
 const fizzbuzz = (arr) => {
   // Solution code here...
+
+  const newArray = [];
+
+  arr.forEach(num => {
+
+    if( num % 3 === 0 && num % 5 === 0){
+      newArray.push('Fizz Buzz');
+    }else if (num % 5 === 0){
+      newArray.push('Buzz');
+    }else if (num % 3 === 0){
+      newArray.push('Fizz');
+    }else{
+      newArray.push(num);
+    }
+
+  });
+
+  return newArray;
+
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -214,7 +233,7 @@ describe('Testing challenge 6', () => {
   });
 });
 
-xdescribe('Testing challenge 7', () => {
+describe('Testing challenge 7', () => {
   const inputs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
   test('It should print out messages or numbers', () => {
